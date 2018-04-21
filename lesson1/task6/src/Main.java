8a9d465f7de0fe629bfcce8e88aa72a84a402d82
jacky105135 <jacky105135@gmail.com>
2018-04-21 12:50:40 +0800

    2018/4/21 commit

--- lesson1/task6/src/Main.java
+++ lesson1/task6/src/Main.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
 
   public static void main(String[] args) {
-    //put your code here
+    Scanner scan = new Scanner(System.in);
+    int a = Integer.parseInt(scan.nextLine());
+      if(a % 4 != 0 ){
+        System.out.println("Regular");
+      }else {
+        if (a % 100 == 0 && a % 400 !=0){
+          System.out.println("Regular");
+        }else if (a %100 != 0){
+          System.out.println("Leap");
+        }else if (a % 400 == 0){
+          System.out.println("Leap");
+        }
+      }
   }
 }
